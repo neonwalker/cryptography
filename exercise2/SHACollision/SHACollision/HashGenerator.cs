c6f3ba5d47a854a0d8004a2f711752c63a441f69
YUSIF Pasha-zada <yxp975@cs.bham.ac.uk>
2019-11-05 20:11:17 +0000

    Collision algorithm

---
+++ exercise2/SHACollision/SHACollision/HashGenerator.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace SHACollision
+{
+    public class HashGenerator
+    {
+        public Dictionary<ulong, string> CalculatedHashes { get; set; }
+
+        public (string, string) Calculate(uint count)
+        {
+            Clear();
+            using (SHA1Managed sha1 = new SHA1Managed())
+            {
+                string key = "";
+                byte[] hash;
+                while (CalculatedHashes.Count != count)
+                {
+                    key = KeyGenerator.GetUniqueKey(15);
+                    hash = sha1.ComputeHash(Encoding.ASCII.GetBytes(key));
+                    var sb = new StringBuilder(hash.Length * 2);
+
+                    foreach (byte b in hash)
+                    {
+                        sb.Append(b.ToString("X2"));
+                    }
+                    string firstPart = sb.ToString().Substring(0, 6);
+                    ulong value = Convert.ToUInt64(firstPart, 16);
+                    if (!CalculatedHashes.TryAdd(value, key))
+                    {
+                        return (key, CalculatedHashes[value]);
+                    }
+                }
+            }
+            return (null, null);
+        }
+
+        private void Clear()
+        {
+            CalculatedHashes.Clear();
+            GC.Collect();
+        }
+
+        public HashGenerator()
+        {
+            CalculatedHashes = new Dictionary<ulong, string>();
+        }
+    }
+}
---
+++ exercise2/SHACollision/SHACollision/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace SHACollision
+{
+    class Program
+    {
+        static string ByteToString(byte[] value)
+        {
+            var sb = new StringBuilder(value.Length * 2);
+
+            foreach (byte b in value)
+            {
+                sb.Append(b.ToString("X2"));
+            }
+            return sb.ToString();
+        }
+
+        static void Main(string[] args)
+        {
+            HashGenerator generator = new HashGenerator();
+            while (true)
+            {
+                var values = generator.Calculate(180000000);
+                if (values.Item1 != null)
+                {
+                    Console.WriteLine(values.Item1);
+                    Console.WriteLine(values.Item2);
+                    return;
+                }
+            }
+        }
+    }
+}
